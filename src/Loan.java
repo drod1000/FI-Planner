c790b6968f475c9a6cd34b5a246bc92996819461
Daniel Rodriguez <drod2793@gmail.com>
2017-05-12 11:49:05 -0600

    Loan: adds initial fields and constructor

---
+++ src/Loan.java
@@ -0,0 +1,11 @@
+public class Loan {
+    static final double INTEREST_RATE = .06;
+    static final int NUMBER_OF_PAYMENTS = 240;
+    private final double initialAmount;
+    private double principalRemaining;
+
+    public Loan(double initialAmount) {
+        this.initialAmount = initialAmount;
+        this.principalRemaining = initialAmount;
+    }
+}
