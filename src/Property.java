6ea045179923c385a844c10156c2000cef541bcc
Daniel Rodriguez <drod2793@gmail.com>
2017-05-12 11:36:24 -0600

    Property: adds initial fields and constructor

---
+++ src/Property.java
@@ -0,0 +1,10 @@
+public class Property {
+    final static double APPRECIATION_RATE = .03;
+    private double value;
+    private double capRate;
+
+    public Property(double value, double capRate) {
+        this.value = value;
+        this.capRate = capRate;
+    }
+}
