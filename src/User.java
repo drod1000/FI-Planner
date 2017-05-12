14de7a7310c2a545dd4c5b0254fcbdb5725140a0
Daniel Rodriguez <drod2793@gmail.com>
2017-05-12 11:15:06 -0600

    User: adds initial fields and constructor

---
+++ src/User.java
@@ -0,0 +1,10 @@
+public class User {
+    private String name;
+    private double annualSalary;
+    private double cashOnHand = 0;
+
+    public User(String name, double annualSalary) {
+        this.name = name;
+        this.annualSalary = annualSalary;
+    }
+}
