2fb0ad253e813ce1d89a893ee697d1f5fd554ea0
Daniel Rodriguez <drod2793@gmail.com>
2017-05-12 12:46:58 -0600

    Main: prompts for user info

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import model.User;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Prompter prompter = new Prompter();
+        User user = prompter.getUserInfo();
     }
 }
