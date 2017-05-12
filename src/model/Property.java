07729063bbb4c19dc052fafd616ca42630412123
Daniel Rodriguez <drod2793@gmail.com>
2017-05-12 13:56:38 -0600

    Property: adds yearly cash flow method

--- src/model/Property.java
+++ src/model/Property.java
@@ -4,10 +4,16 @@ public class Property {
     static final double APPRECIATION_RATE = .03;
     // TODO: 5/12/17 Add depreciation expense 
     private double value;
+    private final double purchasePrice;
     private final double capRate;
 
-    public Property(double value, double capRate) {
-        this.value = value;
+    public Property(double purchasePrice, double capRate) {
+        this.value = purchasePrice;
+        this.purchasePrice = purchasePrice;
         this.capRate = capRate;
+    }
+
+    public double yearlyCashFlow() {
+        return purchasePrice * capRate;
     }
 }
