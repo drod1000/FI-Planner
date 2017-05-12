cb59cf07b3ebdb905d7a9e41938f6fb23f4e2483
Daniel Rodriguez <drod2793@gmail.com>
2017-05-12 11:40:36 -0600

    Strategy: adds initial fields and constructor

---
+++ src/Strategy.java
@@ -0,0 +1,13 @@
+public class Strategy {
+    private final double savingsRate;
+    private final double capRate;
+    private final double downPaymentPercentage;
+    private final int buyingWindow;
+
+    public Strategy(double savingsRate, double capRate, double downPaymentPercentage, int buyingWindow) {
+        this.savingsRate = savingsRate;
+        this.capRate = capRate;
+        this.downPaymentPercentage = downPaymentPercentage;
+        this.buyingWindow = buyingWindow;
+    }
+}
