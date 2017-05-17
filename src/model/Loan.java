8e21b4f7d0f7a2c06bba3d327068eea0d08cca72
Daniel Rodriguez <drod2793@gmail.com>
2017-05-17 12:20:05 -0600

    Loan: adds payment field

--- src/model/Loan.java
+++ src/model/Loan.java
@@ -5,9 +5,12 @@ public class Loan {
     static final int NUMBER_OF_PAYMENTS = 240;
     private final double initialAmount;
     private double principalRemaining;
+    private final double payment;
 
     public Loan(double initialAmount) {
         this.initialAmount = initialAmount;
         this.principalRemaining = initialAmount;
+        this.payment = ((INTEREST_RATE / 12) * initialAmount) /
+                (1 - Math.pow((1 + INTEREST_RATE/12), -NUMBER_OF_PAYMENTS));
     }
 }
